95ac42ed3d4f8b9774ddb7cc08865026da1a9cde
Tom <610658631@qq.com>
2025-07-20 18:16:05 +0800

    修改
    修改账号

--- photo/README.md
+++
@@ -1,2 +0,0 @@
-#  music photo
-This is a simple album of music used by many people in the short video field
--- photo/js/kanbanniang.js
+++
@@ -1,9 +0,0 @@
-    L2Dwidget.init({"display": {
-        "superSample": 2,
-        "width": 200,
-        "height": 400,
-             "position": "right",
-                 "hOffset": 0,
-        "vOffset": 0
-          }
-     });
